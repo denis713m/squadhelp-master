73bc8d277450b1995eee7791d4ba74501bc665e7
denis713m <57364006+denis713m@users.noreply.github.com>
2020-04-02 23:08:25 +0300

    Artickles - grid variant

--- client/src/components/Articles/Articles.js
+++ client/src/components/Articles/Articles.js
@@ -5,15 +5,32 @@ import articles from "../../articles";
 const Articles = () => {
     return (
         <div className={styles.articlesMainContainer}>
-            {articles.map((article, index) => {
-                return (
-                    <article className={styles.ColumnContainer} key={`article${index}`}>
-                        <h4 className={styles.headerArticle}>{article.title}</h4>
-                        <p className={styles.article}>
-                            {article.body}
-                        </p>
-                    </article> )}
-            )}
+            <div className={styles.ColumnContainer}>
+                {articles.map((article, index) => {
+                        if (index < 2)
+                            return (
+                                <article key={`article${index}`}>
+                                    <h4 className={styles.headerArticle}>{article.title}</h4>
+                                    <p className={styles.article}>
+                                        {article.body}
+                                    </p>
+                                </article>)
+                    }
+                )}
+            </div>
+            <div className={styles.ColumnContainer}>
+                {articles.map((article, index) => {
+                        if (index >= 2)
+                            return (
+                                <article  key={`article${index}`}>
+                                    <h4 className={styles.headerArticle}>{article.title}</h4>
+                                    <p className={styles.article}>
+                                        {article.body}
+                                    </p>
+                                </article>)
+                    }
+                )}
+            </div>
         </div>
     );
 };
