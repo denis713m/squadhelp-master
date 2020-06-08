244f4b4e74a84f5ba3cd27903542413bde7982da
denis713m <57364006+denis713m@users.noreply.github.com>
2020-06-08 23:56:20 +0300

    add to DasshboardPage link to ModeratorPage

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import styles from './Dashboard.module.sass';
 import EventBadgeTimer from '../../components/EventBadgeTimer/EventBadgeTimer';
+import ModeratorDashboard from '../../components/ModeratorDashboard/ModeratorDashboard';
 
 
 const Dashboard = (props) => {
@@ -20,20 +21,27 @@ const Dashboard = (props) => {
                         <EventBadgeTimer key={'eventBadge'} />,
                         <CustomerDashboard key={'dashboard'} history={history} match={props.match}/>
                     ]
-                    :
+                    :role === CONSTANTS.CREATOR ?
                     <CreatorDashboard history={history} match={props.match}/>
+                    :
+                    <ModeratorDashboard/>
             }
-            <div className={styles.btnContainer}>
-            <Link to='/Transactions' style={{ textDecoration: 'none' }}>
-                <div className={styles.button}>Go to my transactions</div>
-            </Link>
             {
-                role === CONSTANTS.CUSTOMER &&
-                <Link to='/EventsTimer' style={{ textDecoration: 'none' }} target={'blank'}>
-                    <div className={styles.button}>Events</div>
-                </Link>
+                role !== CONSTANTS.MODERATOR &&
+                <div className={styles.btnContainer}>
+                    <Link to='/Transactions' style={{ textDecoration: 'none' }}>
+                        <div className={styles.button}>Go to my transactions</div>
+                    </Link>
+                    {
+                        role === CONSTANTS.CUSTOMER &&
+                        <Link to='/EventsTimer' style={{ textDecoration: 'none' }} target={'blank'}>
+                            <div className={styles.button}>Events</div>
+                        </Link>
+                    }
+                </div>
+
             }
-            </div>
+
         </div>
     );
 };
