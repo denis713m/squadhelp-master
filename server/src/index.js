d4d59c99d15105f418526ff72da7d548342d674c
denis713m <57364006+denis713m@users.noreply.github.com>
2020-09-18 22:21:21 +0300

    fixup! fixup! add sessionActiveController

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
 require('./dbMongo/mongoose');
 require('./boot/configureSocketIO');
 require('./boot/ErrorArchive');
-
+const sessionCache = require('./controllers/activeUsersController');
+sessionCache.startSessionCacheControl();
 console.log('Server is ready');
 
 
